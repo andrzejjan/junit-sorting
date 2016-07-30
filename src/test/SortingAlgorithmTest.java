7ace5cbaf8f6cec56b78fec501d3caf8bad342bd
Wojtek <wojtek@alx>
2016-07-30 12:00:19 +0200

    testy Wojtka

--- src/test/SortingAlgorithmTest.java
+++ src/test/SortingAlgorithmTest.java
@@ -1,5 +1,29 @@
 package test;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import main.SortingAlgorithms;
+
 public class SortingAlgorithmTest {
 
+	private SortingAlgorithms classUnderTest;
+	
+	@Before
+	public void before() {
+		classUnderTest = new SortingAlgorithms();
+	}
+	
+	@Test
+	public void test1() {
+		double[] tab = {3, 2, 7, 4, 9};
+		double[] wzorzec = {2, 3, 4, 7, 9};
+		
+		classUnderTest.sortAsc(tab, true);
+		
+		for (int i = 0; i < tab.length; i++) {
+			Assert.assertTrue(tab[i] == wzorzec[i]);
+		}
+	}
 }
