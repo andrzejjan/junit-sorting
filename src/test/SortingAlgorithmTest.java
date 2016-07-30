afdfb5c346a78799747f5edd3ef6742b7155b737
KubaF <Kuba@alx>
2016-07-30 12:05:34 +0200

    Added tests for SortingAlgorithms

--- src/test/SortingAlgorithmTest.java
+++ src/test/SortingAlgorithmTest.java
@@ -1,5 +1,31 @@
 package test;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import main.SortingAlgorithms;
+
 public class SortingAlgorithmTest {
 
+	private SortingAlgorithms classUnderTest;
+
+	@Before
+	public void setUp() {
+		classUnderTest = new SortingAlgorithms();
+	}
+
+	@Test
+	public void test1() {
+		double[] tab1 = { 2.5, 6, 3.1, 1.9, 1.5 };
+		double[] tab2 = { 1.5, 1.9, 2.5, 3.1, 6 };
+
+		classUnderTest.sortAsc(tab1, true);
+				
+		for (int i = 0; i < tab1.length; i++) {
+			Assert.assertTrue(tab1[i] == tab2[i]);
+		}
+	}
+	
+	
 }
